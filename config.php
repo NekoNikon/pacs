14694bb4147ea2069180bdd8e3d5508a9a84f50a
Nikon Dolgushin <31705309+NekoNikon@users.noreply.github.com>
2022-02-26 11:54:00 +0600

    Update config.php
    delete

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
     session_start();
     require_once "classes/dcm4cheeAPI.php";
     $config = array(
-        "host"=>"http://89.218.233.58",
-        "port"=>"8080",
-        "auth"=>"https://89.218.233.58:8843/auth/realms/dcm4che/protocol/openid-connect/token/",
-        "secret"=>'5df47d2e-c976-49a5-bfcf-b50615691fc4'
+        "host"=>"",
+        "port"=>"",
+        "auth"=>"",
+        "secret"=>''
     );
     $server = new DCM4CHEEAPI($config);
